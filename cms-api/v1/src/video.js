a918614f46652d76333cbf0bc1d0ddc7b25060c0
rcrooks <rcrooks@brightcove.com>
2015-09-19 18:20:02 -0400

    add fields

--- cms-api/v1/src/video.js
+++ cms-api/v1/src/video.js
@@ -37,10 +37,17 @@
  * @apiSuccess (Response Fields) {Number} duration video duration in milliseconds
  * @apiSuccess (Response Fields) {String} digital_master_id asset id of the digital master
  * @apiSuccess (Response Fields) {String} Economics whether video is AD_ENABLED (used by the Smart Player, not by the Brightcove Player)
+ * @apiSuccess (Response Fields) {String} folder_id id for the folder the video belongs to
  * @apiSuccess (Response Fields) {Object} geo map of geo-filtering properties
  * @apiSuccess (Response Fields) {String} geo.countries=null array of ISO 3166 list of 2-letter codes(https://www.iso.org/obp/ui/)
  * @apiSuccess (Response Fields) {Boolean} geo.exclude_countries=false if true, country array is treated as a list of countries excluded from viewing
  * @apiSuccess (Response Fields) {Boolean} geo.restricted=false whether geo-restriction is enabled for thie video
+ * @apiSuccess (Response Fields) {Object} images map of image maps
+ * @apiSuccess (Response Fields) {Object} poster map of poster properties
+ * @apiSuccess (Response Fields) {String} poster.asset_id asset id for the poster
+ * @apiSuccess (Response Fields) {Object[]} poster.sources array of poster source maps
+ * @apiSuccess (Response Fields) {Object[]} poster.sources.src URL for a poster source image
+ * @apiSuccess (Response Fields) {String} poster.src URL for the default poster source image
  * @apiSuccess (Response Fields) {Object} link map of scheduling properties
  * @apiSuccess (Response Fields) {String} link.text text for the link
  * @apiSuccess (Response Fields) {String} link.url URL for the link
