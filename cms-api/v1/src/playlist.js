d0df359aca7f80562da39052a02fcace68e931be
rcrooks <rcrooks@brightcove.com>
2015-09-20 12:57:37 -0400

    add create playlist

--- cms-api/v1/src/playlist.js
+++ cms-api/v1/src/playlist.js
@@ -19,8 +19,8 @@
  * @apiparam (URL Parameters) {String} [q] search string - see [search guide](http://docs.brightcove.com/en/video-cloud/cms-api/guides/search-videos.html#combinesearchcriteria) for details. Only _search by tags_ is available for playlists.
  * @apiparam (URL Parameters) {String} [sort] field to sort results by; if absent and there is a search string, results are sorted by relevance or if there is no search string, results are sorted by updated_at descending
  *
- * @apiParamExample {String} Search Example:
- *     https://cms.api.brightcove.com/v1/accounts/57838016001/videos?q=tags:nature,name:nature
+ * @apiParamExample {String} Search for Playlists Example:
+ *     https://cms.api.brightcove.com/v1/accounts/57838016001/playlists?q=tags:nature
  *
  * @apiSuccess (Response Field) {String} account_id Video Cloud account id
  * @apiSuccess (Response Field) {DateString} created_at date/time created
@@ -121,6 +121,89 @@
  * @apiError (Error 4xx) {json} RESOURCE_NOT_FOUND 404: The api couldn't find the resource you requested
  * @apiError (Error 4xx) {json} INVALID_SORT 400: sort parameter specified and invalid field
  * @apiError (Error 4xx) {json} INVALID_SEARCH 400: search string invalid (may not have been URI-encoded)
+ *
+ * @apiErrorExample {json} 404 Error Response
+ *     HTTP/1.1 404 Not Found
+ *     [
+ *         {
+ *             "error_code": "RESOURCE_NOT_FOUND"
+ *         }
+ *     ]
+ *
+ *
+ */
+
+// create playlist
+
+/**
+ * @api {post} /accounts/:account_id/playlists Create a Playlist
+ * @apiName Create a Playlist
+ * @apiGroup Playlist
+ * @apiVersion 1.0.0
+ *
+ * @apiDescription Create a new playlist
+ * for the account
+ *
+ * @apiHeader {String} Content-Type Content-Type: application/json
+ * @apiHeader {String} Authorization Authorization: Bearer access_token (see [Getting Access Tokens](http://docs.brightcove.com/en/video-cloud/oauth-api/guides/get-token.html))
+ *
+ * @apiParam (Path Parameters) {Number} account_id Video Cloud account ID.
+ *
+ * @apiparam (Request Body Fields) {Number} [limit=10] number of videos to return
+ * @apiparam (Request Body Fields) {Number} [offset=0] number of videos to skip in the response
+ * @apiparam (Request Body Fields) {String} [q] search string - see [search guide](http://docs.brightcove.com/en/video-cloud/cms-api/guides/search-videos.html#combinesearchcriteria) for details. Only _search by tags_ is available for playlists.
+ * @apiparam (Request Body Fields) {String} [sort] field to sort results by; if absent and there is a search string, results are sorted by relevance or if there is no search string, results are sorted by updated_at descending
+ *
+ * @apiParamExample {json} Create Playlist Example:
+ *    {
+ *        "description" : "My first bird playlist",
+ *        "name" : "Bird Videos",
+ *        "reference_id" : "bird-playlist-1",
+ *        "type" : "EXPLICIT,
+ *        "video_ids" : ["123456789" , "987654321", "999888777666"]
+ *    }
+ *
+ * @apiSuccess (Response Field) {String} account_id Video Cloud account id
+ * @apiSuccess (Response Field) {DateString} created_at date/time created
+ * @apiSuccess (Response Field) {String} description playlist description
+ * @apiSuccess (Response Field) {Boolean} favorite whether playlist is in favorites list
+ * @apiSuccess (Response Field) {String} id the playlist id
+ * @apiSuccess (Response Field) {String} name the playlist name
+ * @apiSuccess (Response Field) {String} reference_id the playlist reference id
+ * @apiSuccess (Response Field) {String} type the playlist type: EXPLICIT or smart playlist type
+ * @apiSuccess (Response Field) {DateString} updated_at date/time last modified
+ * @apiSuccess (Response Field) {String[]} video_ids array of video ids (EXPLICIT playlists only)
+ * @apiSuccess (Response Field) {String} search search string to retrieve the videos (smart playlists only)
+ *
+ * @apiSuccessExample {json} Success Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "account_id": "1752604059001",
+ *         "created_at": "2015-08-31T15:57:34.885Z",
+ *         "description": null,
+ *         "favorite": falso,
+ *         "id": "4452341376001",
+ *         "name": "OutLearn Demo Playlist",
+ *         "reference_id": null,
+ *         "type": "EXPLICIT",
+ *         "updated_at": "2015-08-31T15:57:52.437Z",
+ *         "video_ids": [
+ *             "4454723119001",
+ *             "4454629913001",
+ *             "4454629914001",
+ *             "4454620115001",
+ *             "4454620114001",
+ *             "4454620113001",
+ *             "4454620112001"
+ *         ]
+ *     }
+ *
+ * @apiError (Error 4xx) {json} UNAUTHORIZED 401: Authentication failed - check to make sure your client credentials were correct for the access token
+ * @apiError (Error 4xx) {json} RESOURCE_NOT_FOUND 404: The api couldn't find the resource you requested
+ * @apiError (Error 4xx) {json} BAD_VALUE 400: The JSON could not be parsed
+ * @apiError (Error 4xx) {json} REFERENCE_ID_IN_USE 409: The specified reference id is already in use
+ * @apiError (Error 4xx) {json} ILLEGAL_FIELD 409: Spelling error or other use of non-existent field
+ * @apiError (Error 4xx) {json} VALIDATION_ERROR 409: the JSON data was not valid; error messages vary depending on the problem
  *
  * @apiErrorExample {json} 404 Error Response
  *     HTTP/1.1 404 Not Found
