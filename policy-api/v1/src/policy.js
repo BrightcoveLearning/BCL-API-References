f694e0d021010f0514624974b313d04585bfb893
rcrooks <rcrooks@brightcove.com>
2015-09-25 08:51:58 -0400

    policy api

--- policy-api/v1/src/policy.js
+++ policy-api/v1/src/policy.js
@@ -55,84 +55,31 @@
  *          ]
  *      }
  *
- * @apiSuccess (Response Fields) {String} text_tracks.mime_type mime-type for the track
- * @apiSuccess (Response Fields) {String} text_tracks.label label for the track
- * @apiSuccess (Response Fields) {Boolean} text_tracks.default whether this is the default track
- * @apiSuccess (Response Fields) {DateString} updated_at when the video was last modified
- * @apiSuccess (Response Fields) {Object} ad_keys=null map of key/value pairs for ad requests
+ * @apiSuccess (Response Fields) {String} key_string the policy key
+ * @apiSuccess (Response Fields) {Object[]} policies array of policy maps
+ * @apiSuccess (Request Body Fields (Full Format)) {Object} policies.pattern the logical pattern for specifying accounts or domains allowed or denied access to the Playback API with this key
+ * @apiSuccess (Request Body Fields (Full Format)) {String} policies.pattern.logical-operator the logical operator is used to match accounts or domains; operators allowed are `=`, `!=`, `contains?`, and `not-contains?`; logical operators may be combined using `and` or `or`
+ * @apiSuccess (Request Body Fields (Full Format)) {String} policies.effect whether domains/accounts matching the pattern should be allowed or denied access to the Playback API
  *
  * @apiSuccessExample {json} Success Response:
- *     HTTP/1.1 200 OK
+ *    HTTP/1.1 200 OK
  *    {
- *      "description": "A small friendly bird...",
- *      "poster_sources": [
- *        {
- *          "src": "http://brightcove.vo.llnwd.net/v1/unsecured/ *    media/57838016001/201502/1437/57838016001_4084480075001_Bird-Chickadee-vs. *    jpg?pubId=57838016001&videoId=4084164751001"
- *        }
- *      ],
- *      "tags": [
- *        "air",
- *        "bird",
- *        "nature"
- *      ],
- *      "cue_points": [],
- *      "custom_fields": {},
- *      "account_id": "57838016001",
- *      "sources": [
- *        {
- *          "avg_bitrate": 1108347,
- *          "width": 640,
- *          "duration": 8240,
- *          "size": 1141597,
- *          "stream_name": "mp4:57838016001/201502/1437/57838016001_4084480003001_Bird-Chickadee. *    mp4&1442937600000&9b6865b5469c425d6ae0e8329aee4295",
- *          "codec": "H264",
- *          "asset_id": "4084480003001",
- *          "container": "MP4",
- *          "height": 344,
- *          "app_name": "rtmp://brightcove.fcod.llnwd.net/a500/v1/uds/rtmp"
- *        },
- *        {
- *          "avg_bitrate": 1108347,
- *          "width": 640,
- *          "src": "http://brightcove.vo.llnwd.net/v1/uds/ *    pd/57838016001/201502/1437/57838016001_4084480003001_Bird-Chickadee. *    mp4?pubId=57838016001&videoId=4084164751001",
- *          "size": 1141597,
- *          "height": 344,
- *          "duration": 8240,
- *          "container": "MP4",
- *          "codec": "H264",
- *          "asset_id": "4084480003001"
- *        },
- *        {
- *          "type": "application/x-mpegURL",
- *          "src": "https://secure.brightcove.com/services/mobile/streaming/index/master. *    m3u8?videoId=4084164751001&pubId=57838016001&secure=true",
- *          "container": "M2TS",
- *          "codec": "H264"
- *        }
- *      ],
- *      "name": "Chickadee",
- *      "reference_id": "Bird_Chickadee.mp4_1425064251252",
- *      "long_description": null,
- *      "duration": 8240,
- *      "published_at": "2015-02-27T19:10:55.425Z",
- *      "text_tracks": [],
- *      "updated_at": "2015-02-27T19:11:57.808Z",
- *      "thumbnail": "http://brightcove.vo.llnwd.net/v1/unsecured/ *    media/57838016001/201502/1437/57838016001_4084480007001_Bird-Chickadee-th. *    jpg?pubId=57838016001&videoId=4084164751001",
- *      "poster": "http://brightcove.vo.llnwd.net/v1/unsecured/ *    media/57838016001/201502/1437/57838016001_4084480075001_Bird-Chickadee-vs. *    jpg?pubId=57838016001&videoId=4084164751001",
- *      "link": null,
- *      "id": "4084164751001",
- *      "ad_keys": null,
- *      "thumbnail_sources": [
- *        {
- *          "src": "http://brightcove.vo.llnwd.net/v1/unsecured/ *    media/57838016001/201502/1437/57838016001_4084480007001_Bird-Chickadee-th. *    jpg?pubId=57838016001&videoId=4084164751001"
- *        }
- *      ],
- *      "created_at": "2015-02-27T19:10:55.425Z"
- *    }
- *
+ *        "key-string": "BCpkADawqM0tR9WJhqqyg4t8NgSulRVnfHyh6cL_U0m7RaoIq19WWR-8EPiWY1ift8zHF6Z3sfTyuXv6LY8bfTAfvzVLb1TrwGTOBJGPwWJ9dJUkny7lUoN1ygk",
+ *        "policies": [
+ *            {
+ *                "effect": "deny",
+ *                "pattern": {
+ *                    "!=": [
+ *                        "[request.params.account-id]",
+ *                        "57838016001"
+ *                    ]
+ *                }
+ *            }
+ *        ]
+ *    } *
  * @apiError (Error 4xx) {json} UNAUTHORIZED 401: Authentication failed; check to make sure your policy key is correct
  * @apiError (Error 4xx) {json} RESOURCE_NOT_FOUND 404: The api couldn't find the resource you requested
  * @apiError (Error 4xx) {json} NOT_AVAILABLE 400: The resource you are requesting is temporarily unavailable
- * @apiError (Error 4xx) {json} FORBIDDEN 403: The asset cannot be retrieved because: 1) DOMAIN: the video is restricted from playing on the current domain; 2) CLIENT_IP: the video is restricted at the current IP address; 3) CLIENT_GEO: the video is restricted from playing in the current geo region
  * @apiError (Error 5xx) {json} UNKNOWN 500: Issue in Brightcove system; try again later.
  * @apiError (Error 5xx) {json} TIMEOUT 500: Server likely too busy; try again later.
  *
@@ -147,7 +94,7 @@
  *
  */
 
-// get video by reference id
+// get policy
 
 /**
  * @api {get} /accounts/:account_id/videos/ref:reference_ID Get Video by Reference ID
