0cbc181779ed2ba3b0dc6c1d3f3788a9e3a7fa68
rcrooks <rcrooks@brightcove.com>
2015-09-27 17:54:22 -0400

    more aapi

--- analytics-api/v1/src/report.js
+++ analytics-api/v1/src/report.js
@@ -15,9 +15,11 @@
  * @apiparam (URL Parameters) {Number} [limit=10] number of videos to return
  * @apiparam (URL Parameters) {Number} [offset=0] number of videos to skip in the response
  * @apiparam (URL Parameters) {String} [sort=video_view] field to sort results by
+ * @apiparam (URL Parameters) {String} [fields=video_view] fields to return for items; see [Dimensions and Fields](http://docs.brightcove.com/en/video-cloud/analytics-api/getting-started/api-overview.html#metrics) for the available fields for each dimension
  * @apiparam (URL Parameters) {String="account","city","country","region","date","date-time","device_os","device_type","player","referrer_domain","destination_domain","search_terms","source_type","video"} dimensions one or more dimensions to report on; see [Multiple Dimensions](http://docs.brightcove.com/en/video-cloud/analytics-api/getting-started/api-overview.html#reportDimensions) for which combined dimensions are supported
  * @apiparam (URL Parameters) {String="dimension==value"} [where] one or more dimension==value pairs to filter the results; see [Where Filters](http://docs.brightcove.com/en/video-cloud/analytics-api/getting-started/api-overview.html#filterValues) for details
  * @apiparam (URL Parameters) {String} [from="(30 days before now)"] Start time for the period covered by the report &mdash; epoch time in milliseconds or a date in the format `yyyy-mm-dd` (such as `2013-09-26`)
+ * @apiparam (URL Parameters) {String} [to="now"] End time for the period covered by the report &mdash; epoch time in milliseconds or a date in the format `yyyy-mm-dd` (such as `2013-09-26`)
  *
  * @apiParamExample {String} Video Dimension Report Example:
  *     https://analytics.api.brightcove.com/v1/data?accounts=57838016001&dimensions=video&where=video==4442677263001&from=2015-08-01&to=2015-08-31&fields=video_view,video_impression,video
