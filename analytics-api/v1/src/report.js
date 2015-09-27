80b981788d28118c0f1dbabb8f5390de0113e0ca
rcrooks <rcrooks@brightcove.com>
2015-09-27 16:46:06 -0400

    fix param

--- analytics-api/v1/src/report.js
+++ analytics-api/v1/src/report.js
@@ -14,7 +14,7 @@
  * @apiParam (URL Parameters) {Number} account_ids one or more Video Cloud account IDs separated by commas
  * @apiparam (URL Parameters) {Number} [limit=10] number of videos to return
  * @apiparam (URL Parameters) {Number} [offset=0] number of videos to skip in the response
- * @apiparam (URL Parameters) {String} [sort] field to sort results by; if absent and there is a search string, results are sorted by relevance or if there is no search string, results are sorted by updated_at descending
+ * @apiparam (URL Parameters) {String} [sort=video_view] field to sort results by
  * @apiparam (URL Parameters) {String="account","city","country","region","date","date-time","device_os","device_type","player","referrer_domain","destination_domain","search_terms","source_type","video"} dimensions one or more dimensions to report on; see [Multiple Dimensions](http://docs.brightcove.com/en/video-cloud/analytics-api/getting-started/api-overview.html#reportDimensions) for which combined dimensions are supported
  * @apiparam (URL Parameters) {String="dimension==value"} [where] one or more dimension==value pairs to filter the results; see [Where Filters](http://docs.brightcove.com/en/video-cloud/analytics-api/getting-started/api-overview.html#filterValues) for details
  * @apiparam (URL Parameters) {String} [from="(30 days before now)"] Start time for the period covered by the report &mdash; epoch time in milliseconds or a date in the format `yyyy-mm-dd` (such as `2013-09-26`)
