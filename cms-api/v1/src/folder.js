d984902dbb83249e768ab715a0feb9234f03b53d
rcrooks <rcrooks@brightcove.com>
2015-09-27 08:27:47 -0400

    update folder descriptions

--- cms-api/v1/src/folder.js
+++ cms-api/v1/src/folder.js
@@ -77,7 +77,7 @@
  * @apiGroup Folders
  * @apiVersion 1.0.0
  *
- * @apiDescription Gets list of folders for the account
+ * @apiDescription Gets information about a folder
  *
  * @apiHeader {String} Content-Type Content-Type: application/json
  * @apiHeader {String} Authorization Authorization: Bearer access_token (see [Getting Access Tokens](http://docs.brightcove.com/en/video-cloud/oauth-api/guides/get-token.html))
@@ -440,6 +440,7 @@
  *     ]
  *
  */
+
 // add video to folder
 
 /**
@@ -448,7 +449,7 @@
  * @apiGroup Folders
  * @apiVersion 1.0.0
  *
- * @apiDescription Update the folder name
+ * @apiDescription Add a video to a folder
  *
  * @apiHeader {String} Content-Type Content-Type: application/json
  * @apiHeader {String} Authorization Authorization: Bearer access_token (see [Getting Access Tokens](http://docs.brightcove.com/en/video-cloud/oauth-api/guides/get-token.html))
@@ -486,3 +487,97 @@
  *     ]
  *
  */
+
+// remove video from folder
+
+/**
+ * @api {delete} /accounts/:account_id/folders/:folder_id/videos/:video_id Remove Video from Folder
+ * @apiName Remove Video from Folder
+ * @apiGroup Folders
+ * @apiVersion 1.0.0
+ *
+ * @apiDescription Remove a video from a folder
+ *
+ * @apiHeader {String} Content-Type Content-Type: application/json
+ * @apiHeader {String} Authorization Authorization: Bearer access_token (see [Getting Access Tokens](http://docs.brightcove.com/en/video-cloud/oauth-api/guides/get-token.html))
+ *
+ * @apiParam (Path Parameters) {Number} account_id Video Cloud account ID
+ * @apiParam (Path Parameters) {String} folder_id the folder ID
+ * @apiParam (Request Body Fields) {String} name name of the folder (must be unique in the account)
+ *
+ * @apiParamExample {json} Update Folder Example:
+ *     https://cms.api.brightcove.com/v1/accounts/57838016001/folders/560039dfe4b0471bef470c47/videos/4442677263001
+ *
+ * @apiSuccess (Response Fields) {String} account_id Video Cloud account id
+ * @apiSuccess (Response Fields) {DateString} created_at date/time folder created
+ * @apiSuccess (Response Fields) {DateString} updated_at date/time folder last modified
+ * @apiSuccess (Response Fields) {String} id system id for the folder
+ * @apiSuccess (Response Fields) {String} name folder name
+ * @apiSuccess (Response Fields) {String} video_count number of videos in the folder
+ *
+ * @apiSuccessExample {json} Success Response:
+ *    HTTP/1.1 204 No Content
+ *
+ *
+ * @apiError (Error 4xx) {json} UNAUTHORIZED 401: Authentication failed; check to make sure your client credentials were correct for the access token
+ * @apiError (Error 4xx) {json} RESOURCE_NOT_FOUND 404: The api couldn't find the resource you requested
+ * @apiError (Error 4xx) {json} NOT_AVAILABLE 400: The resource you are requesting is temporarily unavailable
+ * @apiError (Error 5xx) {json} UNKNOWN 500: Issue in Brightcove system; try again later.
+ * @apiError (Error 5xx) {json} TIMEOUT 500: Server likely too busy; try again later.
+ *
+ * @apiErrorExample {json} 404 Error Response
+ *     HTTP/1.1 404 Not Found
+ *     [
+ *         {
+ *             "error_code": "RESOURCE_NOT_FOUND"
+ *         }
+ *     ]
+ *
+ */
+
+// delete folder
+
+/**
+ * @api {delete} /accounts/:account_id/folders/:folder_id Delete Folder
+ * @apiName Delete Folder
+ * @apiGroup Folders
+ * @apiVersion 1.0.0
+ *
+ * @apiDescription Delete a folder
+ *
+ * @apiHeader {String} Content-Type Content-Type: application/json
+ * @apiHeader {String} Authorization Authorization: Bearer access_token (see [Getting Access Tokens](http://docs.brightcove.com/en/video-cloud/oauth-api/guides/get-token.html))
+ *
+ * @apiParam (Path Parameters) {Number} account_id Video Cloud account ID
+ * @apiParam (Path Parameters) {String} folder_id the folder ID
+ * @apiParam (Request Body Fields) {String} name name of the folder (must be unique in the account)
+ *
+ * @apiParamExample {json} Update Folder Example:
+ *     https://cms.api.brightcove.com/v1/accounts/57838016001/folders/560039dfe4b0471bef470c47
+ *
+ * @apiSuccess (Response Fields) {String} account_id Video Cloud account id
+ * @apiSuccess (Response Fields) {DateString} created_at date/time folder created
+ * @apiSuccess (Response Fields) {DateString} updated_at date/time folder last modified
+ * @apiSuccess (Response Fields) {String} id system id for the folder
+ * @apiSuccess (Response Fields) {String} name folder name
+ * @apiSuccess (Response Fields) {String} video_count number of videos in the folder
+ *
+ * @apiSuccessExample {json} Success Response:
+ *    HTTP/1.1 204 No Content
+ *
+ * @apiError (Error 4xx) {json} UNAUTHORIZED 401: Authentication failed; check to make sure your client credentials were correct for the access token
+ * @apiError (Error 4xx) {json} RESOURCE_NOT_FOUND 404: The api couldn't find the resource you requested
+ * @apiError (Error 4xx) {json} NOT_AVAILABLE 400: The resource you are requesting is temporarily unavailable
+ * @apiError (Error 5xx) {json} UNKNOWN 500: Issue in Brightcove system; try again later.
+ * @apiError (Error 5xx) {json} TIMEOUT 500: Server likely too busy; try again later.
+ *
+ * @apiErrorExample {json} 404 Error Response
+ *     HTTP/1.1 404 Not Found
+ *     [
+ *         {
+ *             "error_code": "RESOURCE_NOT_FOUND"
+ *         }
+ *     ]
+ *
+ */
+
