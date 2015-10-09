2b2b9a49052cbf487457f8ec1cbc83c9e3f90859
rcrooks <rcrooks@brightcove.com>
2015-10-09 19:13:00 -0400

    updates

--- once-media-managment-api/v1/src/catalog.js
+++ once-media-managment-api/v1/src/catalog.js
@@ -219,27 +219,26 @@
  *
  * @apiParam (Path Parameters) {String} domain_id The domain id for your Once account
  * @apiParam (Path Parameters) {String} catalog_id The catalog id
+ * @apiParam (Path Parameters) {String} rendition_id The rendition id
  *
- * @apiParamExample {Url} Get Catalog Request Example:
- *    https://api.unicornmedia.com/media-management-api/domain/4eca7ac5-3954-416d-bb23-e65aa511b85a/catalogs/62191781-a933-49d6-831f-83bdf51a26ac
+ * @apiParamExample {Url} Get Catalog Rendition Request Example:
+ *    https://api.unicornmedia.com/media-management-api/domain/4eca7ac5-3954-416d-bb23-e65aa511b85a/catalogs/62191781-a933-49d6-831f-83bdf51a26ac/renditions/076ea1a2-a35b-11e4-bfdb-005056837bc7
  *
- * @apiSuccess (Response Fields) {Object[]} results Array of rendition objects
- * @apiSuccess (Response Fields) {String} results.id The id for the rendition
- * @apiSuccess (Response Fields) {String} results.name The rendition name
+ * @apiSuccess (Response Fields) {String} id The id for the rendition
+ * @apiSuccess (Response Fields) {String} name The rendition name
+ * @apiSuccess (Response Fields) {Number} width The frame width for the rendition in pixels
+ * @apiSuccess (Response Fields) {Number} height The frame height for the rendition in pixels
  *
  * @apiSuccessExample {json} Success Response:
  *    HTTP/1.1 200 OK
  *    {
- *        "results": [
- *            {
- *                "id": "076ea1a2-a35b-11e4-bfdb-005056837bc7",
- *                "name": "OV2 Once 1200 ZC 640x360 1104.96 29.97 B31"
- *            },
- *            {
- *                "id": "225bd8bb-a577-11e4-bfdb-005056837bc7",
- *                "name": "OV2 Once 2000 ZC 960x540 1872.128 29.97 M31"
- *            }
- *        ]
+ *        "id": "076ea1a2-a35b-11e4-bfdb-005056837bc7",
+ *        "name": "OV2 Once 1200 ZC 640x360 1104.96 29.97 B31",
+ *        "width": 640,
+ *        "height": 360,
+ *        "videoBitRate": 1104,
+ *        "audioBitRate": 96,
+ *        "codecsValue": "mp4a.40.2,avc1.42001f"
  *    }
  *
  * @apiError (Error 4xx) {json} UNAUTHORIZED 401: Authentication failed; check to make sure your policy key is correct
