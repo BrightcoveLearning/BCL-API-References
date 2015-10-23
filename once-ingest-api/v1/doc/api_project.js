d58fa45efafe67bb41f90ca38eb12df08d54d874
rcrooks <rcrooks@brightcove.com>
2015-10-23 13:44:55 -0400

    another update

--- once-ingest-api/v1/doc/api_project.js
+++ once-ingest-api/v1/doc/api_project.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-define({  "name": "Once Ingest API",  "version": "1.0.0",  "description": "Reference for the Once Ingest API, used to ingest media assets into the Once system.",  "title": "Once Ingest API Reference",  "url": "https://api.unicornmedia.com/ingest-api",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-10-23T17:42:32.536Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
-=======
-define({  "name": "Once Ingest API",  "version": "1.0.0",  "description": "Reference for the Once Ingest API, used to ingest media assets into the Once system.",  "title": "Once Ingest API Reference",  "url": "https://api.unicornmedia.com/ingest-api",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-10-23T14:59:27.322Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
->>>>>>> origin/master
+define({  "name": "Once Ingest API",  "version": "1.0.0",  "description": "Reference for the Once Ingest API, used to ingest media assets into the Once system.",  "title": "Once Ingest API Reference",  "url": "https://api.unicornmedia.com/ingest-api",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-10-23T17:44:24.229Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
