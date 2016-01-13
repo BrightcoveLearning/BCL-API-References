3cd4020852f447f5be69b0fa06fa1816d998056d
rcrooks <rcrooks@brightcove.com>
2016-01-13 06:12:17 -0500

    fix for cms api reference

--- cms-api/v1/src/playlist.js
+++ cms-api/v1/src/playlist.js
@@ -20,7 +20,7 @@
  *
  * @apiParam (Path Parameters) {String} account_id Video Cloud account ID.
  *
- * @apiParam (URL Parameters) {Number} [limit=10] number of videos to return
+ * @apiParam (URL Parameters) {Number} [limit=20] number of videos to return
  * @apiParam (URL Parameters) {Number} [offset=0] number of videos to skip in the response
  * @apiParam (URL Parameters) {String} [q] search string - see [search guide](http://docs.brightcove.com/en/video-cloud/cms-api/guides/search-videos.html#combinesearchcriteria) for details. Only _search by tags_ is available for playlists.
  * @apiParam (URL Parameters) {String} [sort] field to sort results by; if absent and there is a search string, results are sorted by relevance or if there is no search string, results are sorted by updated_at descending
