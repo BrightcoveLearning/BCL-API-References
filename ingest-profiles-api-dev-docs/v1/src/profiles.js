bb013f6a443eb60161e96e9157f66de242f96938
rcrooks <rcrooks@brightcove.com>
2016-02-12 15:46:44 -0500

    updating IP API doc

--- ingest-profiles-api-dev-docs/v1/src/profiles.js
+++ ingest-profiles-api-dev-docs/v1/src/profiles.js
@@ -60,8 +60,8 @@
  * @apiParam (Request Body Fields) {String} renditions.watermarks.url URL for the watermark image
  * @apiParam (Request Body Fields) {String} renditions.watermarks.width width in pixels or percent of frame width; e.g. `20` or `10%`
  * @apiParam (Request Body Fields) {String} renditions.watermarks.height height in pixels or percent of frame width; e.g. `20` or `10%`
- * @apiParam (Request Body Fields) {String} renditions.watermarks.x distance from left edge to center of image as pixels or precent of frame width; e.g. `20` or `10%`
- * @apiParam (Request Body Fields) {String} renditions.watermarks.y distance from top edge to center if image as pixels or percent of frame height; e.g. `20` or `10%`
+ * @apiParam (Request Body Fields) {String} renditions.watermarks.x distance from left edge to center of image as pixels or precent of frame width; e.g. `20` or `10%` (use negative values like `-10` to position the watermark relative to the right edge)
+ * @apiParam (Request Body Fields) {String} renditions.watermarks.y distance from top edge to center if image as pixels or percent of frame height; e.g. `20` or `10%` (use negative values like `-10` to position the watermark relative to the bottom edge)
  * @apiParam (Request Body Fields) {Object[]} packages array of package maps for DRM (see [Content Security](http://docs.brightcove.com/en/video-cloud/ingest-profiles-api/guides/drm.html))
  * @apiParam (Request Body Fields) {String[]} packages.drm for MPEG-DASH, array of DRM types to apply, e.g. `["widevine", "playready"]`
  * @apiParam (Request Body Fields) {String} packages.package_type for MPEG-DASH, the package type is `dash`; for other formats, the package type is the DRM type, e.g. `widevine`
@@ -90,6 +90,8 @@
  *          "watermarks": [
  *            {
  *              "width": "20%",
+ *              "x": -10,
+ *              "y": -20,
  *              "url": "http://learning-services-media.brightcove.com/images/bc_logo.png"
  *            }
  *          ]
