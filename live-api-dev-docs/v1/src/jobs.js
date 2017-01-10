56d967d8c3d38971e2f58b57fb8e1c6eb0785558
Robert Crooks <rcrooks@brightcove.com>
2017-01-10 13:32:59 -0500

    Live api update

--- live-api-dev-docs/v1/src/jobs.js
+++ live-api-dev-docs/v1/src/jobs.js
@@ -15,6 +15,14 @@
  * @apiParam (Request Body Fields) {Boolean} [ad_insertion=false] Setting this parameter to true will enable server side ad insertion (SSAI) on the job. Current support includes, DFP, Freewheel, or any VAST 2.0/3.0 ad tags.
  * @apiParam (Request Body Fields) {String} region AWS region list specified for the account.
  * @apiParam (Request Body Fields) {Number{0-1800}} [reconnect_time=30] The time, in seconds, to wait for a stream to reconnect to the encoder.
+ * @apiParam (Request Body Fields) {Object} [encryption] Encryption to apply to the stream.
+ * @apiParam (Request Body Fields) {String="aes-128"} encryption.method The encryption method to use.
+ * @apiParam (Request Body Fields) {String="internal","external"} encryption.type The encryption type, depending on whether an internal or external key server will be used.
+ * @apiParam (Request Body Fields) {String} [encryption.key] The encryption key - either a `key` or a `passphrase` is required; if the `type` is `external`, `key` is required.
+ * @apiParam (Request Body Fields) {String} [encryption.passphrase] The encryption key - either a `key` or a `passphrase` is required
+ * @apiParam (Request Body Fields) {Boolean} [encryption.key_rotation=false] Whether to use key rotation
+ * @apiParam (Request Body Fields) {Number} [encryption.rotate_every=10] Interval for key rotation in video segments
+ * @apiParam (Request Body Fields) {String} [encryption.external_url] The URL for the external encryption key - this field is required if you specify `type` as `external`, and the external key must match the `key` value
  * @apiParam (Request Body Fields) {Number{0-93600}} [event_length=0] The minimum time, in seconds, to keep a live stream available. At any point within the specified event_length you may reconnect to your stream. The event_length setting goes into effect as soon as streaming begins.
  * @apiParam (Request Body Fields) {Number{0-7200}} [live_sliding_window_duration=100] The time, in seconds, to keep in the live DVR manifest. If the stream duration is longer than the window duration, segment references will be removed first in first out. Default is 100 seconds.
  * @apiParam (Request Body Fields) {Number{1-5}} [max_hls_protocol_version=3] Sets the maximum HLS protocol version to use. Special features will be used as available. Default is 3.
@@ -365,6 +373,157 @@
  *                "credentials": "YOUR_CREDENTIALS"
  *            }]
  *    }
+ *
+ * @apiParamExample {json} Standard Live Stream with Internal Encryption Example:
+ *    {
+ *        "live_stream": true,
+ *        "region": "my-region-list",
+ *        "reconnect_time": 20,
+ *        "live_sliding_window_duration": 30,
+ *        "encryption": {
+ *            "method": "aes-128",
+ *            "type": "internal",
+ *            "key":"01234567890123450123456789012345"
+ *        }
+ *        "outputs": [
+ *            {
+ *                "label": "hls720p",
+ *                "live_stream": true,
+ *                "width": 960,
+ *                "height": 540,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 1843,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            },
+ *            {
+ *                "label": "hls480p",
+ *                "live_stream": true,
+ *                "width": 640,
+ *                "height": 360,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 819,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            }
+ *        ]
+ *    }
+ *
+ * @apiParamExample {json} Standard Live Stream with Internal Encryption and Passphrase Example:
+ *    {
+ *        "live_stream": true,
+ *        "region": "my-region-list",
+ *        "reconnect_time": 20,
+ *        "live_sliding_window_duration": 30,
+ *        "encryption": {
+ *            "method": "aes-128",
+ *            "type": "internal",
+ *            "passphrase": "SuperSecret
+ *        }
+ *        "outputs": [
+ *            {
+ *                "label": "hls720p",
+ *                "live_stream": true,
+ *                "width": 960,
+ *                "height": 540,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 1843,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            },
+ *            {
+ *                "label": "hls480p",
+ *                "live_stream": true,
+ *                "width": 640,
+ *                "height": 360,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 819,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            }
+ *        ]
+ *    }
+ *
+ * @apiParamExample {json} Standard Live Stream with Internal Encryption and Rotation Example:
+ *    {
+ *        "live_stream": true,
+ *        "region": "my-region-list",
+ *        "reconnect_time": 20,
+ *        "live_sliding_window_duration": 30,
+ *        "encryption": {
+ *            "method": "aes-128",
+ *            "type": "internal",
+ *            "rotate_every": 5,
+ *            "key_rotation": true,
+ *        }
+ *        "outputs": [
+ *            {
+ *                "label": "hls720p",
+ *                "live_stream": true,
+ *                "width": 960,
+ *                "height": 540,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 1843,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            },
+ *            {
+ *                "label": "hls480p",
+ *                "live_stream": true,
+ *                "width": 640,
+ *                "height": 360,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 819,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            }
+ *        ]
+ *    }
+ *
+ * @apiParamExample {json} Standard Live Stream with External URL and Key Example:
+ *    {
+ *        "live_stream": true,
+ *        "region": "my-region-list",
+ *        "reconnect_time": 20,
+ *        "live_sliding_window_duration": 30,
+ *        "encryption": {
+ *            "method": "aes-128",
+ *            "type": "external",
+ *            "key": "01234567890123456789012345678901",
+ *            "external_url": "https://myserver/mykey/a.key"
+ *        }
+ *        "outputs": [
+ *            {
+ *                "label": "hls720p",
+ *                "live_stream": true,
+ *                "width": 960,
+ *                "height": 540,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 1843,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            },
+ *            {
+ *                "label": "hls480p",
+ *                "live_stream": true,
+ *                "width": 640,
+ *                "height": 360,
+ *                "video_codec": "h264",
+ *                "h264_profile": "main",
+ *                "video_bitrate": 819,
+ *                "segment_seconds": 6,
+ *                "keyframe_interval": 60
+ *            }
+ *        ]
+ *    }
+ *
  *
  * @apiSuccess (Response Fields) {String} id Id for the stream.
  * @apiSuccess (Response Fields) {Object[]} outputs Details on each output rendition of the Live job.
