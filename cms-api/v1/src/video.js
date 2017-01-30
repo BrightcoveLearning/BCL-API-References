ae8658a4a21efcf5d9dabb5ad419fa6ad837ec02
rcrooks <rcrooks@brightcove.com>
2017-01-30 06:45:17 -0500

    update cms api

--- cms-api/v1/src/video.js
+++ cms-api/v1/src/video.js
@@ -1152,7 +1152,7 @@
  * @apiParam (Request Body Fields) {String{..250}} [description] video short description
  * @apiParam (Request Body Fields) {String="AD_supported", "FREE"} [economics="AD_SUPPORTED"] video short description
  * @apiSuccess (Request Body Fields) {String{..1800}} [ad_keys=null] string representing the ad key/value pairs assigned to the video. Key/value pairs are formatted as key=value and are separated by ampersands.
- * @apiParam (Request Body Fields) {Boolean} [drm_disabled] if DRM is enabled for the account, setting this field to `true` will prevent this video from being DRM-protected
+ * @apiParam (Request Body Fields) {Boolean} [drm_disabled] if DRM is enabled for the account, setting this field to `true` will prevent this video from being DRM-protected (this field is only available for accounts enabled for Dynamic Delivery and DRM)
  * @apiParam (Request Body Fields) {String{..5000}} [long_description] video long description
  * @apiParam (Request Body Fields) {String="equirectangular"} [projection=null] Used for 360 video
  * @apiParam (Request Body Fields) {String{..150}} [reference_id] video reference-id (must be unique within the account)
